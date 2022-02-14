5116846966c739c79bb89fe5fd69a710b944dfeb
Isarel Getu <israelgetu101@gmail.com>
2022-02-14 11:26:24 -0600

    Done

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,15 @@
 let array = [28, 43, -12, 30, 4, 0, 12];
 
 // Write your solution below:
+let result = false;
+
+for (let i = 0; i < array.length; i++) {
+  for (let k = i + 1; k < array.length; k++) {
+    console.log("i " + array[i] + " k " + array[k]);
+    if (array[i] + array[k] === 0) {
+      result = true;
+    }
+  }
+}
+
+console.log(result);
